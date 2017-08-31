836959f76d60be112d1fc16d16bf169b6f6a7624
RaTeNo <rateno@mail.ru>
2017-08-31 23:07:43 +0300

    new

--- config/app.php
+++ config/app.php
@@ -139,7 +139,7 @@ return [
     'providers' => [
 
         /*
-         * Laravel Framework Service Providers...
+         * Laravel Framework Service Providers...123123
          */
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
